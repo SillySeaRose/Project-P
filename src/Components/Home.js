c39cb11d38d4ea0eb87b99a8e5c989c2f92172ce
SillySeaRose <adrianosm99@gmail.com>
2023-05-20 16:54:19 +0100

    Menu do website quase feito

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Home.css'
+import menuImage from "./Sprites/BackGroundImages/menu/MenuImage.png"
 
 const Home = () => {
   return (
     <>
-        <div>Home</div>
+        <div className='background'></div>
         <nav id="navegationBar"> 
-            <NavLink to="/about"><li id='buttonAbout'></li></NavLink> 
-            <NavLink to="/charactersPage"><li id="buttonCharacters" ></li></NavLink>
-            <NavLink to="/concepts"><li id="buttonConcepts" ></li> </NavLink>
+            <NavLink className="menuLink" to="/about"><li id='buttonAbout'>About</li></NavLink> 
+            <NavLink className="menuLink" to="/charactersPage"><li id="buttonCharacters" >Characters</li></NavLink>
+            <NavLink className="menuLink" to="/concepts"><li id="buttonConcepts">Concepts</li></NavLink>
         </nav>
+        <img src={menuImage} alt='MenuImage' className='menuImage'></img>
     </>
   )
 }
--- src/Components/StartingPage.js
+++ src/Components/StartingPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./StartingPage.css"
 import { NavLink } from 'react-router-dom'
+import image from "./Sprites/BackGroundImages/characterHoldingStartButtonAndStars.png"
+// import backgroundMoving from "./Sprites/BackGroundImages/starsbackgroundmoving.png"
 
 const StartingPage = () => {
   return (
     <>
     <div className='StartingPageBackground'>
+    <div className='background'></div>
     <h1 id='portefolioTitle'> SillySeaRose Portfolio </h1>
+    <img src={image} alt="CharacterImage" className='CharacterHoldingStart'></img>
     <NavLink to="/home"><div className='startWebSite'>Start</div></NavLink>
     </div>
     </>
