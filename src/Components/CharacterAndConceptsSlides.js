0334bb80290ae40f2520a042a6a25639d5fe4aa0
SillySeaRose <adrianosm99@gmail.com>
2023-05-29 16:39:33 +0100

    v2 of carouseal done

--- src/Components/CharacterAndConceptsSlides.js
+++ src/Components/CharacterAndConceptsSlides.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./CharacterSlides.css"
-// import CharactersAndConcepts from './CharactersAndConcepts';
+
 //We take give a prop called "slides" wich is going to read the array of objects "slides"
 const CharacterSlides = ({slides}) => {
     //Here we will check in wich slide we are in, updating it with the useState Hook  
@@ -16,23 +16,34 @@ const CharacterSlides = ({slides}) => {
         transition: "all ease 0.3s",
         zIndex: "3",
     }
+   
 
     // To advance in the slider
     const nextPicture = () => {
-        //Check the last position of the Id
-        const isLastPicture = currentIndex === slides.lenght -1;
-        //
-        const newIndex = isLastPicture ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
+        //Check the condition, gives a bollean
+        const isLastPicture = currentIndex === slides.length -1;
+        // If the currentIndex reaches the max lenght of the array "slides" he swaps the value of the "currentIndex" to 0
+        if (currentIndex === slides.length - 1){
+            setCurrentIndex(0);
+        }
+        //if the condition of the var "isLastPicture" is false, he will add plus one to the new var "newIndex" having the current index in "currentIndex" because we cant swap values in a const
+        else if (isLastPicture === false){
+            const newIndex = currentIndex + 1;
+            setCurrentIndex(newIndex);
+        }
+        
+        //or
+        /* const newIndex = isLastPicture ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);*/
     }
 
+    
     // To go back in the slider
     const backPicture = () => {
-        //this will check the first position of the Id
+        //Check the condition
         const isFirstPicture = currentIndex === 0;
-        //this will change the position of the index, checking if the lenght of the current is less then the minimun possible and return to the last one. Or simply going back, removing one from the currentIndex
-        const newIndex = isFirstPicture ? slides.lengh -1 : currentIndex - 1;
-        // here we set the New Id we got in the useState
+        //this will change the position of the index, doing the same thing as the function "nextPicture" but in reverse, instead of adding one, removes one or goes back to the last in case we go under  index "0"
+        const newIndex = isFirstPicture ? slides.length -1 : currentIndex - 1;
         setCurrentIndex(newIndex);
     }
 
