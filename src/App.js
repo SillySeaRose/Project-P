c6243e677e8d572c0b26244756c1c897743c5828
SillySeaRose <adrianosm99@gmail.com>
2023-06-01 16:44:21 +0100

    v1 Character following the cursor, done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import CharactersPage from './Components/CharactersPage';
 import Home from './Components/Home';
 import Concepts from './Components/ConceptsPage';
 import StartingPage from './Components/StartingPage';
+import Character from './Components/Character';
 
 
 function App() {
   return (
     <>
-      {/* Usar uma grid para organizar os elementos, bordas, cantos e uma área principal para o conteudo */}
+      {/* 
+        Using a grid to organize the elements, borders, corners and a main area for the content
+      */}
+      <Character/>
+
       <div className="container">
 
-        {/* Área onde se encontra as rotas para os vários links - grid  = 5*/}
+        {/*Area where we will find the routes to the links */}
         <div className="mainarea">
+
             <Routes>
               <Route path='/' element={<StartingPage/>}/>
               <Route path="home" element={<Home/>}/>
@@ -23,24 +29,20 @@ function App() {
               <Route path="charactersPage" element={<CharactersPage/>}/>   
               <Route path='concepts' element={<Concepts/>}/> 
             </Routes> 
+
         </div>
-        {/* Zonas laterais - grid = 2 , 4 , 6 , 8*/}
+        {/* lateral zones - grid = 2 , 4 , 6 , 8*/}
         <div className="top"></div>
         <div className="bottom"></div>
         <div className="right"></div>
         <div className="left"></div>
-        {/* Cantos da interface - grid = 1, 3 , 7 ,9 */}
+        {/* interface corners - grid = 1, 3 , 7 ,9 */}
         <div className="tl"></div>
         <div className="tr"></div>
         <div className="bl"></div>
         <div className="br"></div>
         
       </div>
-          
-          
-      
-
-      
     </>
   );
 }
--- src/Components/Character.js
+++ src/Components/Character.js
@@ -2,21 +2,76 @@ import React from 'react'
 import './Character.css'
 
 const Character = () => {
-    let topMove = 50 
-    //let leftMove = 50 
-    let movementSpeed = 2;
-   
-    document.querySelector("#character")?.addEventListener("keypress", (e) => {
-        if (e.key === 'w'){
-            topMove-= movementSpeed;
-            e.style.top = topMove + "%";
-        }
-    } )
-  
+    const mainCursor = React.useRef(null);
+    const secondaryCursor = React.useRef(null);
+
+    const positionRef = React.useRef({
+        //position of the mouse
+        mouseX: 0,
+        mouseY: 0,
+        // position where we want it to arrive
+        destinationX: 0,
+        destinationY: 0,
+        //distance to get to the arrive place
+        distanceX: 0,
+        distanceY: 0,
+        key: -1
+    })
+    React.useEffect(() => {
+        document.addEventListener("mousemove", (event) => {
+            // cordinates
+            const {clientX, clientY} = event;
+            
+            const mouseX = clientX;
+            const mouseY = clientY;
+
+            positionRef.current.mouseX = mouseX - secondaryCursor.current.clientWidth / 3;
+            positionRef.current.mouseY = mouseY - secondaryCursor.current.clientHeight / 3;
+
+            //way to calculate the place where the "mainCursor" will go
+            mainCursor.current.style.transform = `translate3d(${mouseX - 
+                mainCursor.current.clientWidth / 4}px, ${mouseY -
+                mainCursor.current.clientHeight / 3}px, 0)`;
+        });
+
+        return () => {};
+    }, []);
+
+    React.useEffect(() => {
+        const followMouse = () => {
+
+            positionRef.current.key = requestAnimationFrame(followMouse);
+
+            const {mouseX, mouseY, destinationX,destinationY, distanceX,distanceY} = positionRef.current;
+
+            if(!destinationX | !destinationY){
+                positionRef.current.destinationX = mouseX;
+                positionRef.current.destinationY = mouseY;
+            } else {
+                positionRef.current.distanceX = (mouseX - destinationX) * 0.07;
+                positionRef.current.distanceY = (mouseY - destinationY) * 0.07;
+
+                if (Math.abs(positionRef.current.distanceX) + Math.abs(positionRef.current.distanceY) < 0.1){
+                    positionRef.current.destinationX = mouseX;
+                    positionRef.current.destinationY = mouseY;
+                } else {
+                    positionRef.current.destinationX += distanceX;
+                    positionRef.current.destinationY += distanceY;
+                }
+            }
+
+            secondaryCursor.current.style.transform=`translate3d(${destinationX}px , ${destinationY}px, 0)`
+
+        };
+        //Makes a loop, where its calling always the function to reload
+        followMouse();
+        return () => {};
+    }, []);
+
     return (
         <>
-            <p className='text'>Personagem:</p>
-            <div id = "character"></div>
+            <div className='main-cursor' ref={mainCursor}></div>
+            <div className='secondary-cursor' ref={secondaryCursor}></div>
         </>
   )
 }
